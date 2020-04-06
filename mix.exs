71feed6c8725d0af9dad45cf66367c5f15b4e44d
Alabama Air <alabama.air@gmail.com>
2020-04-06 11:00:06 +0300

    Add bootstrap, slime and ru locale

--- mix.exs
+++ mix.exs
@@ -42,6 +42,7 @@ defmodule Cookpod.MixProject do
       {:phoenix, "~> 1.4.0"},
       {:phoenix_pubsub, "~> 1.1"},
       {:phoenix_ecto, "~> 4.0"},
+      {:phoenix_slime, "~> 0.13.1"},
       {:ecto_sql, "~> 3.0"},
       {:postgrex, ">= 0.0.0"},
       {:phoenix_html, "~> 2.11"},
